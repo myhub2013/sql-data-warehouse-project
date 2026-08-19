8e570ee219b298ab211bb5dd73d8eacaadbf47c4
Brian <3581522+myhub2013@users.noreply.github.com>
2026-08-19 14:13:04 -0700

    Fix OBJECT_ID checks for table existence
    Updated OBJECT_ID checks to include the type parameter for table existence checks.

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -7,7 +7,7 @@
 
 */
 
-IF OBJECT_ID(bronze.crm_cust_info) IS NOT NULL
+IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
   DROP TABLE bronze.crm_cust_info;
 GO
 
@@ -21,7 +21,7 @@ CREATE TABLE bronze.crm_cust_info (
   cst_create_date    DATE
 );
 
-IF OBJECT_ID(bronze.crm_prd_info) IS NOT NULL
+IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
   DROP TABLE bronze.crm_prd_info;
 GO
 
@@ -35,7 +35,7 @@ CREATE TABLE bronze.crm_prd_info (
 	prd_end_dt   DATETIME
 );
 
-IF OBJECT_ID(bronze.crm_sales_details) IS NOT NULL
+IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
   DROP TABLE bronze.crm_sales_details;
 GO
 
@@ -51,7 +51,7 @@ CREATE TABLE bronze.crm_sales_details (
 	sls_price    INT
 );
 
-IF OBJECT_ID(bronze.erp_loc_a101) IS NOT NULL
+IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
   DROP TABLE bronze.erp_loc_a101;
 GO
 
@@ -60,7 +60,7 @@ CREATE TABLE bronze.erp_loc_a101 (
 	cntry   NVARCHAR(50)
 );
 
-IF OBJECT_ID(bronze.erp_cust_az12) IS NOT NULL
+IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
   DROP TABLE bronze.erp_cust_az12;
 GO
 
@@ -70,7 +70,7 @@ CREATE TABLE bronze.erp_cust_az12 (
 	gen   NVARCHAR(50)
 );
 
-IF OBJECT_ID(bronze.erp_px_cat_g1v2) IS NOT NULL
+IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
   DROP TABLE bronze.erp_px_cat_g1v2;
 GO
 
